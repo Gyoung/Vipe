eb80615836bbe00b1121699cb19d3495d7d0c56b
gyoung <652105072@qq.com>
2017-02-18 23:05:30 +0800

    增加service-host项目

---
+++ module/vipe-service-host/src/main/java/com/vipe/host/VipeHost.java
@@ -0,0 +1,8 @@
+package com.vipe.host;
+
+/**
+ * Created by Administrator on 2017/2/18 0018.
+ */
+public class VipeHost {
+    
+}
---
+++ module/vipe-service-host/src/main/java/com/vipe/host/package-info.java
@@ -0,0 +1,5 @@
+/**
+ *  后端主服务进程
+ * Created by Administrator on 2017/2/18 0018.
+ */
+package com.vipe.host;
